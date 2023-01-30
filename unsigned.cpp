c864b8de863bd5d2affd10b087d63392c0ec0057
shubasish <shubasish@recruit.so>
2023-01-31 03:03:01 +0600

    learn basic C++ syntax

---
+++ unsigned.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   /*
+  data type
+  int, long long int, float, double, char, string, bool
+  */
+    
+  /*
+  print unsigned number
+  
+  unsigned int a;
+  a = 6;
+  cout << a;
+  */
+
+  /* 
+  print ASCII code
+
+  char letter = 'a';
+  cout << letter;
+  cout << '\n';
+  cout << 'A';
+  cout << ' ';
+  cout << '0';
+
+  cout << letter << "\n" << "A" << " " << "0";
+  */
+
+  /*
+  swap numbers
+
+  int a, b, temp;
+  cin >> a >> b;
+  temp = a;
+  a = b;
+  b = temp;
+  cout << a << b;
+  */
+
+
+  /* 
+  print sum of integers
+
+  int a, b, p;
+  cin >> a >> b;
+  p = a+b;
+  cout << p << endl;
+  */
+
+  /*
+  print Programming is Pinik!
+  cout << "Programming is Pinik!" << endl;
+  */
+
+  /*
+  print following datatypes
+
+  int i;
+  cin >> i;
+  cout << "the value of the integer i = " << i << endl;
+
+  long long int l;
+  cin >> l;
+  cout << "the value of the long long l = " << l << endl;
+
+  char c;
+  cin >> c;
+  cout << "the value of the character c = " << c << endl;
+
+  float f;
+  cin >> f;
+  cout << "the value of the float f = " << f << endl;
+
+  double d;
+  cin >> d;
+  cout << "the value of the double d = " << d << endl;
+  */
+
+  /*
+  print the following pattern
+  *
+  **
+  ***
+  ****
+  ***** 
+
+  cout << "*" << endl << "**" << endl << "***" << endl << "****" << endl << "*****" << endl;  
+  */
+
+  /*
+  print the following pattern
+  *
+  **
+  ***
+  ****
+  *****
+  Additional conditions:
+  You can use cout at most once in your code!
+  You can use << at most once in your code! 
+
+  cout << " * \n ** \n *** \n **** \n *****" << endl;  
+  
+  */
+
+  /*
+  Take three integers a, b and c as input. Then do some swaps in such a way that,
+  the value of a becomes the value of b
+  the value of b becomes the value of c
+  the value of c becomes the value of a
+
+  int a, b, c, temp;
+  cin >> a >> b >> c;
+  temp = c;
+  c = a;
+  a = b;
+  b = temp;
+  cout << "a = " << a << endl << "b = " << b << endl << "c = " << c << endl;
+
+  */
+  return 0;
+}
